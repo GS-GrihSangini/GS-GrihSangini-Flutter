bc6669d45810a33b3f3228d51984b482787f9963
chirayumit001 <chirayumit001@gmail.com>
2021-09-07 00:23:33 +0530

    Made a basic app with home page

---
+++ android/app/src/main/kotlin/com/example/pakkagsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pakkagsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'forgotpassword.dart';
+import 'signup.dart';
+import 'main.dart';
+import 'drawer.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Screen"),
+      ),
+      drawer: MainDrawer(),
+      //*EDIT REQUIRED- Need to make more user friendy UI*/
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[Text("Tap the maps icon to find local workers")],
+        ),
+      ),
+    );
+  }
+}
+
+class MyLoginPage extends StatefulWidget {
+  const MyLoginPage({Key? key}) : super(key: key);
+
+  @override
+  _MyLoginPageState createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<MyLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
